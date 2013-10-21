2469c131097dc726a5b4e86c1cb4ba13c3b8e578
Alex Brandt <alunduil@alunduil.com>
2013-10-21 18:33:34 -0500

    Add information module.

---
+++ parameters/information.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2013 by Alex Brandt <alunduil@alunduil.com>
+#
+# pmort is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+NAME = 'parameters'
+VERSION = '9999'
+DESCRIPTION = \
+        'Generalized all-in-one parameters module.'
+LONG_DESCRIPTION = \
+        'Allows a single definition of parameters that parses all possible ' \
+        'input sources.  Sources including the environment, configuration ' \
+        'files, and command line arguments.  This library provides a simple ' \
+        'interface that consistently accesses these sources.'
+AUTHOR = 'Alex Brandt'
+AUTHOR_EMAIL = 'alunduil@alunduil.com'
+URL = 'https://github.com/alunduil/parameters'
+LICENSE = 'MIT'
+
+COPY_YEAR = '2013'
