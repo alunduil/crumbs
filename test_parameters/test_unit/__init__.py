93c7a6997d3ed7dd5787f42a4a9c0638a8663e44
Alex Brandt <alunduil@alunduil.com>
2013-10-20 20:53:56 -0500

    Add stub module for unit test suite.

---
+++ test_parameters/test_unit/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2013 by Alex Brandt <alunduil@alunduil.com>
+#
+# parameters is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
