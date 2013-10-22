e4d7b26da71b9ec94dcfeb530fad4e7c90ad8416
Alex Brandt <alunduil@alunduil.com>
2013-10-22 10:37:22 -0500

    Add Parameters construction.
    The Parameters object initializes with a few basic values but otherwise
    doesn't do much of not upon initialization.

--- parameters/__init__.py
+++ parameters/__init__.py
@@ -2,3 +2,21 @@
 #
 # pmort is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+class Parameters(object):
+    def __init__(self):
+        '''Initialize Parameters with the given items.
+
+        This initializes a blank set of parameters to which parameters and
+        configuration files may be added.
+
+        This object keeps track of multiple configuration files and multiple
+        groupings of parameters.
+
+        '''
+
+        self.defaults = {}
+        self.parameters = {}
+        self.configuration_files = {}
+        self.groups = { 'default': {} }
+        self.parsed = False
--- test_parameters/test_unit/__init__.py
+++ test_parameters/test_unit/__init__.py
@@ -2,3 +2,77 @@
 #
 # parameters is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import unittest
+import mock
+
+from parameters import Parameters
+
+class ParametersCreateTest(unittest.TestCase):
+    def test_parameters_create(self):
+        '''Parameters()
+
+        Check that the default properties of Parameters are set correctly.
+
+        Namespaced parameters are dot separated such as the following:
+
+        * default.param
+        * foo.bar
+
+        Properties
+        ----------
+
+        :defaults:            dict mapping the namespaced parameter name to the
+                              default value for the parameter.
+        :parameters:          dict mapping the namespaced parameter name to the
+                              parameter tuple for the parameter.
+        :configuration_files: dict mapping configuration file paths to
+                              configparser for the configuration file.
+        :groups:              dict mapping group names to a dict of parameters
+                              (like the parameters attribute).
+        :parsed:              bool indicating whether full parsing has
+                              occurred.
+
+        '''
+
+        p = Parameters()
+
+        self.assertEqual({}, p.defaults)
+        self.assertEqual({}, p.parameters)
+        self.assertEqual({}, p.configuration_files)
+        self.assertEqual({ 'default': {} }, p.groups)
+        self.assertEqual(False, p.parsed)
+
+class ParametersReadTest(unittest.TestCase):
+    def test_read_environment_only(self):
+        pass
+
+    def test_read_configuration_only(self):
+        pass
+
+    def test_read_argument_only(self):
+        pass
+
+    def test_read_environment_multi(self):
+        pass
+
+    def test_read_configuration_multi(self):
+        pass
+
+    def test_read_argument_multi(self):
+        pass
+
+class ParametersUpdateTest(unittest.TestCase):
+    pass
+
+class ParametersDeleteTest(unittest.TestCase):
+    pass
+
+class ParametersAddParametersTest(unittest.TestCase):
+    pass
+
+class ParametersAddConfigurationFileTest(unittest.TestCase):
+    pass
+
+class ParametersParseParametersTest(unittest.TestCase):
+    pass
