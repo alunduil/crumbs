b7372b68800f39d5e6233ca32b7c83601b4ec178
Alex Brandt <alunduil@alunduil.com>
2013-10-22 19:56:37 -0500

    Add parameter addition to Parameters.
    We need to provide an interface to our users to add a parameters;
    othwerise, what's the point?  This sets up the basics required when
    adding a parameter for basic bookkeeping.  It also initializes the
    argument parser(s) involved and ensures that we know what parameters
    we're looking for.

--- parameters/__init__.py
+++ parameters/__init__.py
@@ -3,8 +3,14 @@
 # pmort is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import logging
+import argparse
+import copy
+
+logger = logging.getLogger(__name__)
+
 class Parameters(object):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initialize Parameters with the given items.
 
         This initializes a blank set of parameters to which parameters and
@@ -13,10 +19,109 @@ class Parameters(object):
         This object keeps track of multiple configuration files and multiple
         groupings of parameters.
 
+        Arguments
+        ---------
+
+        Any passed arguments are picked up by wildcards (*args and **kwargs).
+        These are passed directly to an instance of ArgumentParser for
+        initialization.
+
         '''
+
+        logger.info('initializing Parameters object')
 
         self.defaults = {}
         self.parameters = {}
         self.configuration_files = {}
-        self.groups = { 'default': {} }
+        self.groups = set([ 'default' ])
         self.parsed = False
+
+        self._group_parsers = { 'default': argparse.ArgumentParser(*args, **kwargs) }
+
+    def add_parameter(self, **kwargs):
+        '''Add the specified components as a parameter.
+
+        The components mostly line up with the argparse arguments (outlined
+        below).  There are a couple of extra possible arguments that we utilize
+        and everything else is discarded without being inspected.
+
+        Arguments
+        ---------
+
+        The arguments are lumped into two groups which are inspected for the
+        listed items.  These are passed to ArgumentParser.add_argument after
+        removing the specific items for Parameters.
+
+        Parameters.add_parameter Arguments
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+        :``group``:   Namespace or section (in configuration file terms) for
+                      this parameter.
+        :``options``: The list of options to match for this parameter in argv.
+        :``only``:    Iterable containing the components that this parameter
+                      applies to (i.e. environment, configuration, argument).
+
+        ArgumentParser.add_argument Arguments
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+        :``name or flags``: Read from kwargs[options].
+        :``action``:        The basic type of action to be taken when this
+                            argument is encountered at the command line.
+        :``nargs``:         The number of command-line arguments that should be
+                            consumed.
+        :``const``:         A constant value required by some action and nargs
+                            selections.
+        :``default``:       The value produced if the argument is absent from
+                            the command line.
+        :``type``:          The type to which the command-line argument should
+                            be converted.
+        :``choices``:       A container of the allowable values for the
+                            argument.
+        :``required``:      Whether or not the command-line option may be
+                            omitted (optionals only).
+        :``help``:          A brief description of what the argument does.
+        :``metavar``:       A name for the argument in usage messages.
+        :``dest``:          The name of the attribute to be added to the object
+                            returned by parse_args().
+
+        '''
+
+        parameter_name = max(kwargs['options'], key = len).lstrip('-')
+
+        if 'dest' in kwargs:
+            parameter_name = kwargs['dest']
+
+        group = kwargs.pop('group', 'default')
+        self.groups.add(group)
+
+        parameter_name = '.'.join([ group, parameter_name ]).lstrip('.')
+
+        logger.info('adding parameter %s', parameter_name)
+
+        if self.parsed:
+            logger.warn('adding parameter %s after parse', parameter_name)
+
+        self.parameters[parameter_name] = copy.copy(kwargs)
+        self.parameters[parameter_name]['group'] = group
+
+        logger.info('group: %s', group)
+
+        actions = {
+                'store': kwargs.get('default'),
+                'store_const': kwargs.get('const'),
+                'store_true': False,
+                'store_false': True,
+                'append': [],
+                'append_const': [],
+                'count': 0,
+                }
+
+        self.defaults[parameter_name] = actions[kwargs.get('action', 'store')]
+
+        logger.info('default value: %s', kwargs.get('default'))
+
+        if 'argument' in kwargs.pop('only', [ 'argument' ]):
+            if group not in self._group_parsers:
+                self._group_parsers[group] = self._group_parsers['default'].add_argument_group(group)
+
+            self._group_parsers[group].add_argument(*kwargs.pop('options'), **kwargs)
--- test_parameters/test_unit/__init__.py
+++ test_parameters/test_unit/__init__.py
@@ -4,7 +4,10 @@
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import unittest
-import mock
+import tempfile
+import os
+import sys
+import functools
 
 from parameters import Parameters
 
@@ -40,10 +43,160 @@ class ParametersCreateTest(unittest.TestCase):
         self.assertEqual({}, p.defaults)
         self.assertEqual({}, p.parameters)
         self.assertEqual({}, p.configuration_files)
-        self.assertEqual([ 'default' ], p.groups)
+        self.assertEqual(set([ 'default' ]), p.groups)
         self.assertFalse(p.parsed)
 
+class ParametersAddParametersTest(unittest.TestCase):
+    def setUp(self):
+        self.p = Parameters(conflict_handler = 'resolve')
+
+        self.in_parameters = (
+                {
+                    'group': 'foo',
+                    'options': [ '--bar', '-b' ],
+                    'default': 'baz',
+                    'type': str,
+                    'help': 'assistance is futile',
+                    },
+                {
+                    'group': 'default',
+                    'options': [ '--baz', '-b' ],
+                    'action': 'store_true',
+                    'help': 'assistance is futile',
+                    },
+                {
+                    'options': [ '--qux', '-q' ],
+                    'default': 'foo',
+                    'help': 'assistance is futile',
+                    },
+                {
+                    'options': [ 'foobar' ],
+                    'nargs': '*',
+                    'help': 'assistance is futile',
+                    },
+                {
+                    'options': [ '--foobaz' ],
+                    'dest': 'quxbaz',
+                    },
+                )
+
+        self.defaults = {
+                'foo.bar': 'baz',
+                'default.baz': False,
+                'default.qux': 'foo',
+                'default.foobar': None,
+                'default.quxbaz': None,
+                }
+
+        self.parameters = {
+                'foo.bar': {
+                    'group': 'foo',
+                    'options': [ '--bar', '-b' ],
+                    'default': 'baz',
+                    'type': str,
+                    'help': 'assistance is futile',
+                    },
+                'default.baz': {
+                    'group': 'default',
+                    'options': [ '--baz', '-b' ],
+                    'action': 'store_true',
+                    'help': 'assistance is futile',
+                    },
+                'default.qux': {
+                    'group': 'default',
+                    'options': [ '--qux', '-q' ],
+                    'default': 'foo',
+                    'help': 'assistance is futile',
+                    },
+                'default.foobar': {
+                    'group': 'default',
+                    'options': [ 'foobar' ],
+                    'nargs': '*',
+                    'help': 'assistance is futile',
+                    },
+                'default.quxbaz': {
+                    'group': 'default',
+                    'options': [ '--foobaz' ],
+                    'dest': 'quxbaz',
+                    },
+                }
+
+        self.groups = set([
+                'default',
+                'foo',
+                ])
+
+    def test_add_parameters(self):
+        for parameter in self.in_parameters:
+            self.p.add_parameter(**parameter)
+
+        self.assertEqual(self.defaults, self.p.defaults)
+
+        _ = self.maxDiff
+        self.maxDiff = None
+        self.assertEqual(self.parameters, self.p.parameters)
+        self.maxDiff = _
+
+        self.assertEqual(self.groups, self.p.groups)
+        self.assertFalse(self.p.parsed)
+
+class ParametersAddConfigurationFileTest(unittest.TestCase):
+    pass
+
+class ParametersParseParametersTest(unittest.TestCase):
+    pass
+
 class ParametersReadTest(unittest.TestCase):
+    def setUp(self):
+        self.original_argv0 = sys.argv[0]
+
+        def _():
+            sys.argv[0] = self.original_argv0
+        self.addCleanup(_)
+
+        sys.argv[0] = 'parameters'
+
+        self.p = Parameters()
+
+    def populateEnvironment(self):
+        os.environ['PARAMETERS_ENV_ONLY'] = 'environment_only'
+        os.environ['PARAMETERS_MULTI'] = 'environment_multi'
+
+        def _(name):
+            del os.environ[name]
+
+        self.addCleanup(functools.partial(_, 'PARAMETERS_ENV_ONLY'))
+        self.addCleanup(functools.partial(_, 'PARAMETERS_MULTI'))
+
+    def populateArgumentVector(self):
+        sys.argv.extend([ '--argument-only', 'argument_only' ])
+        sys.argv.extend([ '--multi', 'argument_multi' ])
+
+        def _(name):
+            sys.argv.remove(name)
+
+        self.addCleanup(functools.partial(_, '--argument-only'))
+        self.addCleanup(functools.partial(_, 'argument_only'))
+        self.addCleanup(functools.partial(_, '--multi'))
+        self.addCleanup(functools.partial(_, 'argument_only'))
+
+    def populateConfiguration(self):
+        tmp_fh = tempfile.NamedTemporaryFile()
+        tmp_fh.write(
+                '[default]\n'
+                'configuration_only = configuration_only\n'
+                'multi = configuration_multi\n'
+                )
+
+        tmp_fh.seek(0)
+
+        self.addCleanup(tmp_fh.close)
+
+    def test_read_environment(self):
+        '''Parameters—environment'''
+
+        self.populateEnvironment()
+
     def test_read_environment_only(self):
         pass
 
@@ -61,18 +214,3 @@ class ParametersReadTest(unittest.TestCase):
 
     def test_read_argument_multi(self):
         pass
-
-class ParametersUpdateTest(unittest.TestCase):
-    pass
-
-class ParametersDeleteTest(unittest.TestCase):
-    pass
-
-class ParametersAddParametersTest(unittest.TestCase):
-    pass
-
-class ParametersAddConfigurationFileTest(unittest.TestCase):
-    pass
-
-class ParametersParseParametersTest(unittest.TestCase):
-    pass
