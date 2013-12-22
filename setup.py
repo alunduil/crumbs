ad488b898dc30410f5239da67d78c5605d2ff00e
Alex Brandt <alunduil@alunduil.com>
2013-12-22 12:20:11 -0600

    Removing python-3.2 with the stabilization of 3.3.

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ PARAMS['classifiers'] = [
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries',
