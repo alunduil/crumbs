834d760135175bbd9a35a98f1ba198aa4cf852ed
Alex Brandt <alunduil@alunduil.com>
2014-03-12 15:06:01 -0500

    Update version to 1.1.2.

--- crumbs/information.py
+++ crumbs/information.py
@@ -4,7 +4,7 @@
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 NAME = 'crumbs'
-VERSION = '9999'
+VERSION = '1.1.2'
 DESCRIPTION = 'Generalized all-in-one parameters module.'
 AUTHOR = 'Alex Brandt'
 AUTHOR_EMAIL = 'alunduil@alunduil.com'
