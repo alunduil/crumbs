51e75a934fbe8129cabbbff378f4446f274eebb7
Alex Brandt <alunduil@alunduil.com>
2014-08-01 16:04:46 -0500

    Bump version to 1.2.2.

--- crumbs/information.py
+++ crumbs/information.py
@@ -4,7 +4,7 @@
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 NAME = 'crumbs'
-VERSION = '1.2.1'
+VERSION = '1.2.2'
 DESCRIPTION = 'Generalized all-in-one parameters module.'
 AUTHOR = 'Alex Brandt'
 AUTHOR_EMAIL = 'alunduil@alunduil.com'
