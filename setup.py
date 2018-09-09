ea9107979bf51f00446e27610370e48313ab51c1
Alex Brandt <alunduil@gmail.com>
2018-09-09 09:43:52 -0700

    Simplify setup.py.

--- setup.py
+++ setup.py
@@ -1,80 +1,61 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2015 by Alex Brandt <alunduil@alunduil.com>
-#
-# crumbs is freely distributable under the terms of an MIT-style license.
-# See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import os
 
 from codecs import open
 from setuptools import setup
 
-with open(os.path.join('crumbs', 'information.py'), 'r', encoding = 'utf-8') as fh:
-    exec(fh.read(), globals(), locals())
+readme=""
+with open("README.rst", 'r', encoding="utf-8") as fh:
+    readme=fh.read()
 
-PARAMS = {}
+setup(
+    name="crumbs",
+    version="2.1.0",
+    description="Generalized all-in-one parameters module.",
+    long_descpription=readme,
+    url="https://github.com/alunduil/crumbs",
 
-PARAMS['name'] = NAME  # noqa — provided by exec
-PARAMS['version'] = VERSION  # noqa — provided by exec
-PARAMS['description'] = DESCRIPTION  # noqa — provided by exec
+    author="Alex Brandt",
+    author_email="alunduil@gmail.com",
 
-with open('README.rst', 'r', encoding = 'utf-8') as fh:
-    PARAMS['long_description'] = fh.read()
+    license="MIT",
 
-PARAMS['url'] = URL  # noqa — provided by exec
-PARAMS['author'] = AUTHOR  # noqa — provided by exec
-PARAMS['author_email'] = AUTHOR_EMAIL  # noqa — provided by exec
-PARAMS['license'] = LICENSE  # noqa — provided by exec
-
-PARAMS['classifiers'] = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: Software Development :: Libraries',
-]
-
-PARAMS['keywords'] = [
-    'crumbs',
-    'parameters',
-    'configuration',
-    'environment',
-    'arguments',
-]
-
-PARAMS['packages'] = [
-    'crumbs',
-]
-
-PARAMS['install_requires'] = []
-
-PARAMS['extras_require'] = {
-    'inotify': [
-        'pyinotify',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries",
     ],
-}
 
-PARAMS['test_suite'] = 'nose.collector'
-PARAMS['tests_require'] = [
-    'coverage',
-    'nose',
-]
+    keywords=[
+        "arguments",
+        "configuration",
+        "crumbs",
+        "environment",
+        "parameters",
+    ],
 
-PARAMS['data_files'] = [
-    ('share/doc/{P[name]}-{P[version]}'.format(P = PARAMS), [
-        'README.rst',
-    ]),
-]
+    packages=[
+        "crumbs",
+    ],
 
-setup(**PARAMS)
+    install_requires=[
+        "pyinotify",
+    ],
+
+    tests_require=[
+        "pytest",
+    ],
+)
